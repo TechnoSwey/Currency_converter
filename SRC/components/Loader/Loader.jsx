32b81ff14f48d861914d8af48a5cb2c6f34b3a07
TechnoSwey <technoswey@yandex.ru>
2025-11-28 13:34:16 +0300

    Create Loader.jsx

---
+++ SRC/components/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+import styles from './Loader.module.css';
+
+export const Loader = () => {
+  return (
+    <div className={styles.loaderContainer}>
+      <div className={styles.spinner}></div>
+      <p>Loading exchange rates...</p>
+    </div>
+  );
+};
